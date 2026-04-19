bb60363aea1d992348ee0b0fb91c34aa847dc10a
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-04-18 21:52:59 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,6 @@
 
 set -ouex pipefail
 
-dnf -y install libva-nvidia-driver 
 dnf -y remove \
     scx-scheds \
     scx-tools
@@ -12,6 +11,6 @@ dnf -y copr enable bieszczaders/kernel-cachyos-addons
 dnf install -y \
     --enablerepo="copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons" \
     --allowerasing \
-    libcap-ng libcap-ng-devel cachyos-ksm-settings procps-ng procps-ng-devel uksmd libbpf scx-scheds-git scx-tools-git scx-manager cachyos-settings kerver
+    libcap-ng libcap-ng-devel cachyos-ksm-settings procps-ng procps-ng-devel uksmd libbpf scx-scheds-git scx-tools-git scx-manager
     
 dnf -y copr disable bieszczaders/kernel-cachyos-addons
