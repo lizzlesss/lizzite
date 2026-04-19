722cb249730f233ab2a7f7c6c14ac27acc06dfad
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-04-18 21:47:02 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,16 +7,11 @@ dnf -y remove \
     scx-scheds \
     scx-tools
 
-
 dnf -y copr enable bieszczaders/kernel-cachyos-addons
-dnf -y copr enable myriad-sun/zen-browser
 
-dnf -y install \
-    scx-scheds-git \
-    scx-tools-git \
-    kerver \
-    zen-browser
+dnf install -y \
+    --enablerepo="copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons" \
+    --allowerasing \
+    libcap-ng libcap-ng-devel cachyos-ksm-settings procps-ng procps-ng-devel uksmd libbpf scx-scheds-git scx-tools-git scx-manager cachyos-settings kerver
     
-
 dnf -y copr disable bieszczaders/kernel-cachyos-addons
-dnf -y copr disable myriad-sun/zen-browser
