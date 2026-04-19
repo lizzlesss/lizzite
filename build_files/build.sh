5153259039a7b9cb18d44f017d34915b29e7dc25
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-04-18 22:20:00 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,14 +4,13 @@ set -ouex pipefail
 
 dnf -y remove \
     scx-scheds \
-    scx-tools \
-    kerver
+    scx-tools
 
 dnf -y copr enable bieszczaders/kernel-cachyos-addons
 
 dnf install -y \
     --enablerepo="copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons" \
     --allowerasing \
-    libcap-ng libcap-ng-devel cachyos-ksm-settings procps-ng procps-ng-devel uksmd libbpf scx-scheds-git scx-tools-git scx-manager cachyos-settings kerver
+    libcap-ng libcap-ng-devel cachyos-ksm-settings procps-ng procps-ng-devel uksmd libbpf scx-scheds-git scx-tools-git scx-manager cachyos-settings
     
 dnf -y copr disable bieszczaders/kernel-cachyos-addons
